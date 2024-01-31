1a4901e2604c607bfe75c6becc86ddb30220f001
kkm0406 <sdjmc30412@naver.com>
2024-01-31 22:09:45 +0900

    # 2024.01.31 BOJ s1 행운의 문자열 # 수학, 브루트포스 알고리즘, 백트래킹

--- BOJ/silver1/1342.py
+++ BOJ/silver1/1342.py
@@ -1,5 +1,5 @@
 # 행운의 문자열
-# 수학, 브루트포스 알고리즘, 백트래킹
+
 import sys
 from collections import Counter
 
@@ -7,29 +7,30 @@ input = sys.stdin.readline
 
 s = input().strip()
 n = len(s)
-
+cnt = 0
 # 알파벳 별 개수
 alpha = Counter(s)
 
+
 # 이전 단어, depth
 def dfs(prev, depth):
-    answer = 0
-
-    # 단어 길이와 같은 경우
-    # 인접한 모든 문자가 같지 않은 경우
+    global cnt
+    # 단어 길이 만큼 재귀 -> 인접한 모든 문자가 같이 않은 경우
     if depth == n:
-        return 1
+        cnt += 1
 
+    # 알바펫 순회하며 재배치
     for w in alpha.keys():
-        # 이전 문자와 같거나 해당 문자을 다 쓴 경우
+        # 이전 문자와 같거나, 더 이상 배치할 문자가 없는 경우 continue
         if w == prev or alpha[w] == 0:
             continue
-
+        # 문자 사용
         alpha[w] -= 1
-        answer += dfs(w, depth + 1)
+        # 재귀
+        dfs(w, depth + 1)
+        # 원상태 복귀
         alpha[w] += 1
 
-    return answer
 
-
-print(dfs("", 0))
+dfs("", 0)
+print(cnt)
