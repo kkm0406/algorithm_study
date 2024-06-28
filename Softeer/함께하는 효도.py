1172901d62d049b859424f0ed47fe2206438e25e
kkm0406 <sdjmc30412@naver.com>
2024-06-28 20:19:50 +0900

    # 2024.06.28 Softeer Lv.3 함께하는 효도

--- Softeer/함께하는 효도.py
+++ Softeer/함께하는 효도.py
@@ -1,43 +1,44 @@
-# LV.3 힘께하는 효도
-
 import sys
-
-input = sys.stdin.readline
-
+from itertools import permutations
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-answer = 0
-visited = [[False] * n for _ in range(n)]
-coords = [[[0, 0] for _ in range(4)] for i in range(m)]
-cost = [0] * m
-
-
-def dfs(idx, depth, prev):
-    if depth == 4:
-        result = 0
-        for px, py in prev:
-            result += arr[px][py]
-        if result > cost[idx]:
-            cost[idx] = result
-            for i in range(4):
-                coords[idx][i] = prev[i]
-    else:
-        for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-            nx, ny = prev[-1][0] + dx, prev[-1][1] + dy
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                visited[nx][ny] = True
-                dfs(idx, depth + 1, prev + [[nx, ny]])
-                visited[nx][ny] = False
-
-
-for i in range(m):
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
+pos = []
+for _ in range(m):
     x, y = map(int, input().split())
-    visited[x - 1][y - 1] = True
-    dfs(i, 1, [[x - 1, y - 1]])
-    visited[x - 1][y - 1] = False
-    for a, b in coords[i]:
-        arr[a][b] = 0
+    pos.append((x-1, y-1))
 
-    answer += cost[i]
+dxs = [0,1,0,-1]
+dys = [1,0,-1,0]
 
-print(answer)
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+def dfs(x, y, score, d, path):
+    global mx_score, mx_path
+    visited[x][y] = True
+    score += grid[x][y]
+    path.append((x, y))
+    if d == 3:
+        if mx_score < score:
+            mx_score = score
+            mx_path = path[:]
+        return
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and not visited[nx][ny] and not (nx, ny) in total_path:
+            dfs(nx, ny, score, d+1, path)
+            visited[nx][ny] = False
+            path.pop()
+perm = list(permutations(pos, m))
+ans = 0
+for perm_case in perm:
+    total_path = []
+    total_score = 0
+    for x, y in perm_case:
+        mx_score, mx_path = 0, []
+        dfs(x, y, 0, 0, [])
+        total_path += mx_path
+        total_score += mx_score
+    ans = max(ans, total_score)
+print(ans)
